e28391f7c73fc28441f1fdb13812ef91ca64e4c9
Alex Adusei <alex.adusei@gmail.com>
2016-12-23 06:02:27 -0500

    Add koa-logger for logging endpoints

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import Koa from 'koa'
+import logger from 'koa-logger'
 
 import mainRoutes from './routes/main'
 import userRoutes from './routes/user'
@@ -6,6 +7,7 @@ import userRoutes from './routes/user'
 const app = new Koa()
 const config = require('./config')
 
+app.use(logger())
 app.use(mainRoutes.routes())
 app.use(userRoutes.routes())
 
